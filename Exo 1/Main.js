cdbdf069f48f551eae66c01c15a1466b2200a584
Pwapwate <147376732+Pwapwate@users.noreply.github.com>
2023-10-09 12:07:44 +0200

    Upload exo Bon format

--- Exo 1/Main.js
+++ Exo 1/Main.js
@@ -6,7 +6,7 @@ if (a == b) {
   console.log("C'est égal");
 } else {
   console.log("raté");
-}   e<e
+}   
 
 if (c < b && a + c != 3) {
   console.log("ok");
@@ -14,13 +14,4 @@ if (c < b && a + c != 3) {
   console.log("raté");
 }
 
-switch (a) {
-  case b:
-    console.log("égal a b");
-    break;
-  case c:
-    console.log("égal a c");
-    break;
-  default:
-    console.log("égal a rien");
-}
+
---
+++ Exo 3/Main.js
@@ -0,0 +1,15 @@
+let a = 4;
+let b = 4;
+let c = 3;
+
+
+switch (a) {
+  case b:
+    console.log("égal a b");
+    break;
+  case c:
+    console.log("égal a c");
+    break;
+  default:
+    console.log("égal a rien");
+}
