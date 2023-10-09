3a3e52fadf6191726294bf91bb152328b6445af6
Pwapwate <147376732+Pwapwate@users.noreply.github.com>
2023-10-09 11:57:35 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,26 @@
+let a = 4;
+let b = 4;
+let c = 3;
+
+if (a == b) {
+  console.log("C'est égal");
+} else {
+  console.log("raté");
+}   e<e
+
+if (c < b && a + c != 3) {
+  console.log("ok");
+} else {
+  console.log("raté");
+}
+
+switch (a) {
+  case b:
+    console.log("égal a b");
+    break;
+  case c:
+    console.log("égal a c");
+    break;
+  default:
+    console.log("égal a rien");
+}
