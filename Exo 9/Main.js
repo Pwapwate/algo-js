728f9e5fab10e22b9542367a2ccba60a826dab57
Pwapwate <147376732+Pwapwate@users.noreply.github.com>
2023-10-13 09:36:21 +0200

    Add files via upload
    Ajout correction Exo 9

--- Exo 9/Main.js
+++ Exo 9/Main.js
@@ -6,21 +6,41 @@ class Pokemon {
     this.hp = hp;
     this.luck = luck;
   }
+
   isLucky() {
     return Math.floor(Math.random() * 100) <= this.luck;
   }
 
-  attackPokemon(Pokemon) {
+  attackPokemon(pokemon) {
     if (this.isLucky()) {
-      let damages = this.attack - Pokemon.defense;
-      Pokemon.hp -= damages;
-      console.log(`${this.name} a attaqué ${pokemon.name}, perdant ${damages} hp !`)
+      let damages = this.attack - pokemon.defense;
+      pokemon.hp -= damages;
+      console.log(
+        `${this.name} a attaqué ${pokemon.name}, perdant ${damages} hp ! ${pokemon.name} a désormais ${pokemon.hp}!`
+      );
     } else {
-      console.log(`${this.name} a raté son attaque`);
+      console.log(`${this.name} a raté son attaque !`);
     }
   }
 }
-let Rattataque = new Pokemon("Rattataque", 8, 3, 10, 80);
-let Poichigeon = new Pokemon("Poichigeon", 9, 15, 20, 25);
 
-Poichigeon.attackPokemon(Rattataque)
+// FIN DE LA CLASSE POKEMON
+// Instanciation des pokemons 
+let moustillon = new Pokemon("Moustillon", 10, 5, 30, 75);
+let rondoudou = new Pokemon("Rondoudou", 15, 3, 30, 50);
+
+// Boucle d'attaque et de victoires
+
+while (moustillon.hp > 0 && rondoudou.hp > 0) {
+  moustillon.attackPokemon(rondoudou);
+  if (rondoudou.hp <= 0) {
+    console.log(`Rondoudou a perdu le combat, il est K.O.`);
+    break;
+  }
+  rondoudou.attackPokemon(moustillon);
+  if (moustillon.hp <= 0) {
+    console.log(`Moustillon a perdu le combat, il est K.O.`);
+    break;
+  }
+}
+console.log("Le combat est terminé");
